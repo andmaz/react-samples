eab0f29d53c2ec3874e0c0b64b33d19a8ee7b5b3
andmaz <andmaz.ua@gmail.com>
2019-01-07 02:34:40 +0200

    add highlight

--- task1/src/components/Tile.js
+++ task1/src/components/Tile.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 export default class Tile extends Component {
 
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = { clickCounter: 0 };
+    this.state = { clickCounter: 0, highlight: false };
   }
 
-  clickCounter = 0;
+  handleClick() {
+    this.setState({ 
+      clickCounter: this.state.clickCounter + 1,
+      highlight: false
+    });
 
-  handleClick(e) {
-    //e.target.className 
+    setTimeout(() => this.setState({ highlight: true }));
 
-    this.setState({clickCounter: this.state.clickCounter+1 });
   }
 
   render() {
     return (
-      <div className="tile" onClick={this.handleClick}>
+      <div className={classNames('tile', { 'highlight': this.state.highlight })} onClick={this.handleClick}>
         <div>
-          { (this.state.clickCounter === 0) ? 'not clicked yet' : `clicked: ${this.state.clickCounter} times` }
+          { 
+            (this.state.clickCounter === 0) 
+              ? 'not clicked yet' 
+              : `clicked: ${this.state.clickCounter} times` 
+          }
         </div>
       </div>
     );
